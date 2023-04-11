4fcec2584f5b2f221c0e354f32fc2625f912405a
LEO GOUBARD <goubardleo@gmail.com>
2023-04-11 14:09:19 +0200

    :construction: change the localhost adress to the new api adress

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://image-gen-api.onrender.com/api/v1/dalle', {
           method: 'POST',
           body: JSON.stringify({ prompt: form.prompt }),
           headers: {
@@ -48,7 +48,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://image-gen-api.onrender.com/api/v1/post', {
           method: 'POST',
           body: JSON.stringify(form),
           headers: {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://image-gen-api.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-type': 'application/json',
